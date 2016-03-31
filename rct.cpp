526c14a63a3009f5488eb87c0062e6c920418595
Astr0id <792880387@qq.com>
2016-03-31 14:57:27 +0800

    Finish memory function,the number of words will be displayed in right way.

--- rct.cpp
+++ rct.cpp
@@ -10,7 +10,8 @@ int main()
 	FILE * outPtr;
 	FILE * rePtr;
 	FILE * loPtr;
-	int number,choice,start;
+	int number,choice,start,count=0;
+	bool flag=true;
 	char buffer[30];
 	inPtr=fopen("./wl.txt","r");
 	outPtr=fopen("./res.txt","a");
@@ -18,6 +19,8 @@ int main()
 	loPtr=fopen("./log.txt","r+");
 	start=initialize(loPtr);
 	for(int i=0;i<start;i++)fgets(buffer,sizeof(buffer),inPtr);
+	cout<<"You have already recited "<<start<<" words"<<endl;
+	count=start;
 	if (inPtr == NULL)
 	{
 		cout<<"ERROR!"<<endl;
@@ -26,29 +29,42 @@ int main()
 	{
 		cout<<"How much do you want?"<<endl;
 		cin>>number;
-		for (int i=0;i<number && !feof(inPtr);i++)
+		while(flag)
 		{
-			fgets(buffer,sizeof(buffer),inPtr);
-			cout<<buffer<<endl<<endl
-			<<"1 for Aboslute, 2 for Blurly, 3 for No Idea"
-			<<endl<<endl;
-			cin>>choice;
-			switch(choice)
+			system("clear");
+			for (int i=0;i<number && !feof(inPtr);i++)
 			{
-				case 1:
+				cout<<"You have already recited "<<count<<" words"<<endl;
+				fgets(buffer,sizeof(buffer),inPtr);
+				cout<<buffer<<endl<<endl
+				<<"1 for Aboslute, 2 for Blurly, 3 for No Idea"
+				<<endl<<endl;
+				cin>>choice;
+				switch(choice)
+				{
+					case 1:
 					break;
-				case 2:
+					case 2:
 					fputs(buffer,rePtr);
 					break;
-				case 3:
+					case 3:
 					fputs(buffer,outPtr);
 					break;
-				default:
+					default:
 					break;
+				}
+				count++;
+				fflush(inPtr);
+				system("clear");
 			}
-			fflush(inPtr);
-			system("clear");
+			
+			cout<<"Do you want to continue?"<<endl
+				<<"1 For YES,2 For NO"<<endl;
+			cin>>choice;
+			if(choice==2)flag=false;
 		}
+		fseek(loPtr,0,SEEK_SET);
+		fprintf(loPtr, "%d", count);
 	}
 	fclose(inPtr);
 	fclose(outPtr);
@@ -61,7 +77,6 @@ int initialize(FILE * logP)
 	char ch[20];
 	int num;
 	fgets(ch,sizeof(ch),logP);
-	cout<<"You have already recited "<<ch<<" words"<<endl;
 	num=atoi(ch);
 	return num;
 	
